7680f05acf56a5dc02c2cab8bd9cf672aa1171b7
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2019-05-30 14:50:56 +0200

    add all files and solution to first exercise

---
+++ L6/ex1/main.cpp
@@ -0,0 +1,19 @@
+#include "./main.h"
+#include "./dbgPrint.h"
+
+using namespace std;
+
+int main()
+{
+	CPrinter printer;
+        printer.setDebugLevel(4);
+
+        MSG_PRINT("msg");
+        DBG_PRINT("msg");
+        WRN_PRINT("msg");
+        ERR_PRINT("msg");
+
+        printMessage("Hello makefiles");
+        return 0;
+}
+
---
+++ L6/ex1/main.h
@@ -0,0 +1,10 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include <iostream>
+void printMessage(const std::string& msg)
+{
+	std::cout<<"My message: "<<msg<<std::endl;
+}
+
+#endif
