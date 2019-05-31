62cc5cf817e3dad5b1e3a46010ee96b6164becb0
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2019-05-31 23:38:07 +0200

    tablica.c fixed

--- C7/gdb2018/gdb/tablica.c
+++ C7/gdb2018/gdb/tablica.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
-#include<math.h>
-
-
-int min,max,*tabl, n;
+#include <stdlib.h>
+#include <limits.h>
 
 int minimum(int *tab, int n) {
-    int m =0;
-    int i =0;
-    for (i=0; i<n; i++) {
-        if (m> tabl[i])
-            m = tabl[i];
-    }
+
+    int m = INT_MAX;
+    int i;
+
+    for (i=0; i<n; i++) 
+        if (m > tab[i])
+            m = tab[i];
+
     return m;
 }
 
 int maksimum(int *tab, int n) {
-    int m =0;
-    int i =0;
-    for (i=0; i<n; i++) {
-        if (m< tabl[i])
-            m = tabl[i];
-    }
+
+    int m = INT_MIN;
+    int i;
+
+    for (i=0; i<n; i++) 
+        if (m < tab[i])
+            m = tab[i];
+
     return m;
 }
 
 int sredni(int *tab, int n) {
-    int avg =0;
-    int i =0;
-    for (i=0; i<n; i++) {
-        avg+=tabl[i];
-    }
+
+    int avg = 0;
+    int i;
+
+    for (i=0; i<n; i++)
+        avg += tab[i];
     
     return avg/n;
 }
@@ -37,23 +40,24 @@ int sredni(int *tab, int n) {
 
 int main ()
 {
+    int *tabl, i, n;
+
     printf("Tablice... znajdz wszystkie bledy :)\n");
-    printf("Podaj Dlugosc tablicy od 0 do 100 \n");
+    printf("Podaj dlugosc tablicy od 0 do 100 \n");
     scanf("%d", &n);
     
-    tabl = new int[n];
-    printf("Podaj 1 element tablicy \n");
-    scanf("%d", &tabl[1]);
-    
-    int i=0;
-    for (i=2; i<=n; i++) {
-        printf("Podaj %d element tablicy \n", i);
+    tabl = (int*)malloc(n*sizeof(int));
+ 
+    for (i=0; i<n; i++) {
+        printf("Podaj %d element tablicy \n", i+1);
         scanf("%d", &tabl[i]);
     }
 
     printf("min tablicy to: %d\n", minimum(tabl, n));
     printf("max tablicy to: %d\n", maksimum(tabl, n));
     printf("avg tablicy to: %d\n", sredni(tabl, n));
+
+    free(tabl);
     
     return 0;
 }
