c71676bf8d7ce36e9bea2a46413db90dd4c5881e
Piotr Józefowicz <pn.jozefowicz@gmail.com>
2019-06-03 13:09:36 +0200

    get rid of unnecessary code

--- C8/src_profile/heapproject.cpp
+++ C8/src_profile/heapproject.cpp
@@ -22,22 +22,22 @@ void losujElementy(int count, char* prefix, BinaryMinHeap *queue) {
 	//	sprintf(str, "napis do wyswietlenia: %s %d", prefix, priority);
 		sprintf(str,"%d", priority);
 		
-
 		queue->insert(new InfoElement(strdup(str), priority));
 	}
 }
 
 void usuwajElementy(BinaryMinHeap* q1, BinaryMinHeap* q2) {
 
-	//for (int i = 0; i <= MAX_PRIORITY; i++) {
-		/*while (!q1->isEmpty() && q1->getMinimum()->time < i) {
+	/*for (int i = 0; i <= MAX_PRIORITY; i++) {
+		while (!q1->isEmpty() && q1->getMinimum()->time < i) {
 		//	printf("Minimum to: %s\n", q1->getMinimum()->info);
 			q1->removeMin();
 		}
 		while (!q2->isEmpty() && q2->getMinimum()->time < i) {
 		//	printf("Minimum to: %s\n", q2->getMinimum()->info);
 			q2->removeMin();
-		}*/
+		}
+	}*/
 
 		while(!q1->isEmpty()) {
 			//printf("Minimum to: %s\n", q1->getMinimum()->info);
@@ -82,37 +82,25 @@ int main() {
 	queue1 = new BinaryMinHeap();
 	queue2 = new BinaryMinHeap();
 
-	//char *q1str = "kolejka1";
-	//char *q2str = "kolejka2";
-
 	losujElementy(30000, "kolejka1", queue1);
 	losujElementy(30000, "kolejka2", queue2);
 	
 	printf("Wylosowałem elementy\n");
 
-	
 	printf("Minimum to: %s\n", queue1->getMinimum()->info);
 
 	queue1->removeMin();
 	
-	
-	printf("Usunalem min element\n");
-
 	printf("Minimum to: %s\n", queue1->getMinimum()->info);
 
-	printf("pokazalem minimun\n");
-
 	queue1->removeMin();
-
-	printf("Usunalem min element\n");
 
 	usuwajElementy(queue1,queue2);
-
-	printf("Usunalem elementy\n");
 
 	losujElementy(350000, "kolejka1", queue1);
 	losujElementy(350000, "kolejka2", queue2);
 
 	usuwajCichoElementy(queue1,queue2);
+
 	return 0;
 }
